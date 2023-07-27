f5a2a1d804d543db3d5b6afbc924741a8ae281a8
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-28 01:35:45 +0500

    feat: implement user authentication
    * Restapis: implement endpoints for user login and registeration.
    * linting: implement eslint rules for linting.
    * MVC: implement mvc model for managing.
    * jwt: implement json web authentication.

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const dataBaseConnection = mongoose
-  .connect(
-    "mongodb+srv://salmanaziz:salman999@cluster0.gllatbf.mongodb.net/Instagram?retryWrites=true&w=majority"
-  )
+  .connect(process.env.URL)
   .then(() => {
-    console.log("connected to server");
+    console.log('connected to server')
   })
   .catch(() => {
-    console.log("not connected");
-  });
+    console.log('not connected')
+  })

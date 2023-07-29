c9867baebaf1445ee100c76e91280eb9a99d7d91
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-29 17:04:35 +0500

    feat: implement user post endpoints
    * CRUD: implement user post crud endpoints.

---
+++ routes/Postroutes.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import multer from 'multer'
+import PostOperations from '../controller/Post.js'
+const router = express.Router()
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, '/home/dev/Desktop/Test Project/server/uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '-' + file.originalname)
+	}
+})
+
+const upload = multer({ storage })
+router.post('/addPost', upload.array('images', 5), PostOperations.addPost)
+router.delete('/deletePost', PostOperations.deletePost)
+router.put('/updatePost', upload.array('images', 5), PostOperations.updatePost)
+export default router
